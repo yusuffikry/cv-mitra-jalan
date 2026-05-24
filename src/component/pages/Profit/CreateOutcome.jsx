38fe15b4fd7ca2a8a18f0921613d33a32a081d0f
yusuffikry <yusuffikri77@gmail.com>
2026-05-24 21:08:25 +0800

    Update CreateOutcome and EditOutcome

--- src/component/pages/Profit/CreateOutcome.jsx
+++ src/component/pages/Profit/CreateOutcome.jsx
@@ -138,7 +138,7 @@ export default function CreateOutcome() {
                       <option value="" disabled>-- Pilih Jenis Pengeluaran --</option>
                       <option value="Beban BBM">Beban BBM</option>
                       <option value="Beban Gaji">Beban Gaji</option>
-                      <option value="Beban Gaji">Beban GPS</option>
+                      <option value="Beban GPS">Beban GPS</option>
                       <option value="Pemeliharaan">Pemeliharaan</option>
                       <option value="Angsuran">Angsuran</option>
                       <option value="Operasional">Operasional</option>
